a543d4e5755abf7649f1bd6089e7e9812d953b23
opengl-8080 <opengl-8080@users.noreply.github.com>
2020-11-18 10:37:41 +0900

    HTTPリクエストのメトリクスを汎用ハンドラを使った方法に変更

--- src/main/java/com/nablarch/example/app/metrics/CustomHttpRequestMetricsTagBuilder.java
+++ src/main/java/com/nablarch/example/app/metrics/CustomHttpRequestMetricsTagBuilder.java
@@ -3,14 +3,25 @@ package com.nablarch.example.app.metrics;
 import io.micrometer.core.instrument.Tag;
 import nablarch.fw.ExecutionContext;
 import nablarch.fw.web.HttpRequest;
-import nablarch.integration.micrometer.instrument.handler.http.HttpRequestMetricsTagBuilder;
+import nablarch.integration.micrometer.instrument.handler.HandlerMetricsMetaDataBuilder;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class CustomHttpRequestMetricsTagBuilder implements HttpRequestMetricsTagBuilder {
+public class CustomHttpRequestMetricsTagBuilder implements HandlerMetricsMetaDataBuilder<HttpRequest, Object> {
+
     @Override
-    public List<Tag> build(HttpRequest request, ExecutionContext context, Throwable thrownThrowable) {
+    public String getMetricsName() {
+        return "http.server.request";
+    }
+
+    @Override
+    public String getMetricsDescription() {
+        return "Custom HTTP Request Metrics";
+    }
+
+    @Override
+    public List<Tag> buildTagList(HttpRequest request, ExecutionContext executionContext, Object result, Throwable thrownThrowable) {
         return Arrays.asList(
             Tag.of("hello", "world")
         );
