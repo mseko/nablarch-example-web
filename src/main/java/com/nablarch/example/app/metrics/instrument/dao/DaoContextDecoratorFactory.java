5b2424daac80bc881e50a9983dcfee9cbcf4ef77
opengl-8080 <opengl-8080@users.noreply.github.com>
2020-12-04 09:19:17 +0900

    sql実行時間メトリクス検証

--- src/main/java/com/nablarch/example/app/metrics/instrument/dao/DaoContextDecoratorFactory.java
+++
@@ -1,8 +0,0 @@
-package com.nablarch.example.app.metrics.instrument.dao;
-
-import nablarch.common.dao.DaoContext;
-
-public interface DaoContextDecoratorFactory {
-
-    DaoContext decorate(DaoContext delegate);
-}
--- src/main/java/com/nablarch/example/app/web/action/MetricsAction.java
+++ src/main/java/com/nablarch/example/app/web/action/MetricsAction.java
@@ -1,15 +1,20 @@
 package com.nablarch.example.app.web.action;
 
+import com.nablarch.example.app.entity.Project;
 import io.micrometer.core.instrument.Measurement;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import nablarch.common.dao.EntityList;
+import nablarch.common.dao.UniversalDao;
 import nablarch.core.log.Logger;
 import nablarch.core.log.LoggerManager;
 import nablarch.core.repository.SystemRepository;
 import nablarch.fw.ExecutionContext;
 import nablarch.fw.web.HttpRequest;
 import nablarch.fw.web.HttpResponse;
+
+import java.util.Arrays;
 
 public class MetricsAction {
     private static final Logger LOGGER = LoggerManager.get(MetricsAction.class);
@@ -20,6 +25,54 @@ public class MetricsAction {
             stackoverflow();
         }
 
+        logCountTest(request);
+
+        buildMetrics(context);
+
+        testSqlProcessTime();
+
+        return new HttpResponse("/WEB-INF/view/metrics/index.jsp");
+    }
+
+    private void testSqlProcessTime() {
+        // 画面操作で補完できない部分を無理やり実行させてテスト
+        EntityList<Project> projects = UniversalDao.findAll(Project.class);
+        projects.forEach(project -> {
+            project.setNote("a");
+        });
+        UniversalDao.batchUpdate(projects);
+
+        Project project1 = UniversalDao.findById(Project.class, projects.get(0).getProjectId());
+        Project project2 = UniversalDao.findById(Project.class, projects.get(1).getProjectId());
+        Project project3 = UniversalDao.findById(Project.class, projects.get(2).getProjectId());
+
+        UniversalDao.batchDelete(Arrays.asList(project1, project2, project3));
+    }
+
+    private void buildMetrics(ExecutionContext context) {
+        String metrics;
+        Object registry = SystemRepository.getObject("meterRegistry");
+        if (registry instanceof PrometheusMeterRegistry) {
+            PrometheusMeterRegistry meterRegistry = (PrometheusMeterRegistry) registry;
+            metrics = meterRegistry.scrape();
+        } else if (registry instanceof SimpleMeterRegistry) {
+            StringBuilder sb = new StringBuilder("@@@@@ SimpleMeterRegistry @@@@@\n");
+            SimpleMeterRegistry meterRegistry = (SimpleMeterRegistry) registry;
+            for (Meter meter : meterRegistry.getMeters()) {
+                Meter.Id id = meter.getId();
+                sb.append("[id=").append(id).append("]\n");
+                for (Measurement measurement : meter.measure()) {
+                    sb.append("  ").append(measurement.getStatistic().name()).append('=').append(measurement.getValue()).append('\n');
+                }
+            }
+            metrics = sb.toString();
+        } else {
+            metrics = "none";
+        }
+        context.setRequestScopedVar("metrics", metrics);
+    }
+
+    private void logCountTest(HttpRequest request) {
         String[] loggings = request.getParam("logging");
         if (loggings !=null && 0 < loggings.length) {
             switch (loggings[0]) {
@@ -45,29 +98,6 @@ public class MetricsAction {
                     LOGGER.logError("unknown loggings=" + loggings[0]);
             }
         }
-
-        String metrics;
-        Object registry = SystemRepository.getObject("meterRegistry");
-        if (registry instanceof PrometheusMeterRegistry) {
-            PrometheusMeterRegistry meterRegistry = (PrometheusMeterRegistry) registry;
-            metrics = meterRegistry.scrape();
-        } else if (registry instanceof SimpleMeterRegistry) {
-            StringBuilder sb = new StringBuilder("@@@@@ SimpleMeterRegistry @@@@@\n");
-            SimpleMeterRegistry meterRegistry = (SimpleMeterRegistry) registry;
-            for (Meter meter : meterRegistry.getMeters()) {
-                Meter.Id id = meter.getId();
-                sb.append("[id=").append(id).append("]\n");
-                for (Measurement measurement : meter.measure()) {
-                    sb.append("  ").append(measurement.getStatistic().name()).append('=').append(measurement.getValue()).append('\n');
-                }
-            }
-            metrics = sb.toString();
-        } else {
-            metrics = "none";
-        }
-        context.setRequestScopedVar("metrics", metrics);
-
-        return new HttpResponse("/WEB-INF/view/metrics/index.jsp");
     }
 
     private void stackoverflow() {
