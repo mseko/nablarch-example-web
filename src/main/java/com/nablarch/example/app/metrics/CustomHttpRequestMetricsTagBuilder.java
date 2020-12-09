672a836266f3fa2c2eeea524ca48d126b4b16c68
opengl-8080 <opengl-8080@users.noreply.github.com>
2020-12-09 11:07:46 +0900

    汎用JMX MeterBinder を使ったメトリクス収集を追加

--- src/main/java/com/nablarch/example/app/metrics/CustomHttpRequestMetricsTagBuilder.java
+++ src/main/java/com/nablarch/example/app/metrics/CustomHttpRequestMetricsTagBuilder.java
@@ -3,27 +3,17 @@ package com.nablarch.example.app.metrics;
 import io.micrometer.core.instrument.Tag;
 import nablarch.fw.ExecutionContext;
 import nablarch.fw.web.HttpRequest;
+import nablarch.integration.micrometer.instrument.MetricsMetaData;
 import nablarch.integration.micrometer.instrument.handler.HandlerMetricsMetaDataBuilder;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class CustomHttpRequestMetricsTagBuilder implements HandlerMetricsMetaDataBuilder<HttpRequest, Object> {
 
     @Override
-    public String getMetricsName() {
-        return "http.server.request";
-    }
-
-    @Override
-    public String getMetricsDescription() {
-        return "Custom HTTP Request Metrics";
-    }
-
-    @Override
-    public List<Tag> buildTagList(HttpRequest request, ExecutionContext executionContext, Object result, Throwable thrownThrowable) {
-        return Arrays.asList(
-            Tag.of("hello", "world")
-        );
+    public MetricsMetaData build(HttpRequest param, ExecutionContext executionContext, Object o, Throwable thrownThrowable) {
+        return new MetricsMetaData("http.server.request", "Custom HTTP Request Metrics", Arrays.asList(
+                Tag.of("hello", "world")
+        ));
     }
 }
--- src/main/java/com/nablarch/example/app/metrics/CustomMeterBinderListProvider.java
+++ src/main/java/com/nablarch/example/app/metrics/CustomMeterBinderListProvider.java
@@ -3,18 +3,53 @@ package com.nablarch.example.app.metrics;
 import com.nablarch.example.app.metrics.instrument.logger.NablarchLoggerMetrics;
 import io.micrometer.core.instrument.binder.MeterBinder;
 import nablarch.core.log.basic.LogLevel;
+import nablarch.core.repository.initialization.Initializable;
 import nablarch.integration.micrometer.DefaultMeterBinderListProvider;
+import nablarch.integration.micrometer.instrument.MetricsMetaData;
+import nablarch.integration.micrometer.instrument.binder.jmx.JmxGaugeMetrics;
+import nablarch.integration.micrometer.instrument.binder.jmx.MBeanAttributeCondition;
 import nablarch.integration.micrometer.instrument.binder.logging.LogCountMetrics;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
-public class CustomMeterBinderListProvider extends DefaultMeterBinderListProvider {
+public class CustomMeterBinderListProvider extends DefaultMeterBinderListProvider implements Initializable {
+
+    private DataSource dataSource;
 
     @Override
     protected List<MeterBinder> createMeterBinderList() {
         return Arrays.asList(
-            new LogCountMetrics(LogLevel.DEBUG)
+            new LogCountMetrics(LogLevel.DEBUG),
+            new JmxGaugeMetrics(
+                new MetricsMetaData("db.pool.total", "Total DB pool count."),
+                new MBeanAttributeCondition("com.zaxxer.hikari:type=Pool (HikariPool-1)", "TotalConnections")
+            ),
+            new JmxGaugeMetrics(
+                new MetricsMetaData("db.pool.active", "Active DB pool count."),
+                new MBeanAttributeCondition("com.zaxxer.hikari:type=Pool (HikariPool-1)", "ActiveConnections")
+            ),
+            new JmxGaugeMetrics(
+                new MetricsMetaData("thread.count.current", "Current thread count."),
+                // 組み込みTomcatを使う場合(waitt:run)は、ObjectNameが"Tomcat:type=..."になるので注意
+                new MBeanAttributeCondition("Catalina:type=ThreadPool,name=\"http-nio-8080\"", "currentThreadCount")
+            )
         );
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void initialize() {
+        try (Connection con = dataSource.getConnection()) {
+            // HikariCP の MBean 登録は初回コネクション接続時なので、先に接続しておかないと MBean が取れずに警告ログが出力されてしまう
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
